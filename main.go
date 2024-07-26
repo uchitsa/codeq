022996c23ca3457e27ab123cf73ef31377aa30d6
uchitsa <uchitsa@ya.ru>
2024-07-26 19:30:13 +0300

    wip

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main(){
+import (
+	"context"
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+)
 
+const githubToken = "my_github_token"
+
+func main() {
+	ctx := context.Background()
+	tokenSrc := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: githubToken})
+	ocl := oauth2.NewClient(ctx, tokenSrc)
+	_ = github.NewClient(ocl)
 }
